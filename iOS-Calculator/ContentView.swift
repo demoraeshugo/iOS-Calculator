e41b8c5c47aa0838d04701228b8e90018b269ed5
Hugo De Moraes <demoraeshugo@gmail.com>
2020-07-22 09:50:51 -0400

    Initial Commit

---
+++ iOS-Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Calculator
+//
+//  Created by Hugo De Moraes on 7/22/20.
+//  Copyright © 2020 Hugo De Moraes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
